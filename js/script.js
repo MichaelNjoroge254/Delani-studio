80703130a223b91b75bc8819046b19c2ca7de8f8
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-06-21 13:13:02 +0300

    add script

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function (){
+    $("#img1").mouseover(function() {
+       $("#ttt").show();
+       $("#img1").hide();
+
+    });
+    $("#ttt").mouseleave(function() {
+       $("#ttt").hide();
+       $("#img1").show();
+
+    });
+     $("#img2").mouseover(function() {
+       $("#ttt2").show();
+       $("#img2").hide();
+
+    });
+    $("#ttt2").mouseleave(function() {
+       $("#ttt2").hide();
+       $("#img2").show();
+
+    });
+     $("#img3").mouseover(function() {
+       $("#ttt3").show();
+       $("#img3").hide();
+
+    });
+    $("#ttt3").mouseleave(function() {
+       $("#ttt3").hide();
+       $("#img3").show();
+
+    });
+ });
